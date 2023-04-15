5fcad23d3de64b0349f832cca1600f6d5bc86a02
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-04-15 16:36:17 +0530

    added the script for the addition

---
+++ addition.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+################
+# Author : Vikash Kumar Yadav
+# Date   : 15-04-2023
+# Topic  : Addition of the two number 
+################
+
+echo "Enter the first number:"
+read a;
+
+echo "Enter the second number:"
+read b;
+
+echo "The result after addition of the two number is:"
+echo "$a + $b = $((a+b))"
