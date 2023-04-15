feb90b7304f3114f8e4e749111ffa1b89d192293
Vikash KUmar Yadav <yadav621gzp@gmail.com>
2023-04-15 16:37:15 +0530

    added  the script for the hello world

---
+++ hello.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+#####################
+# Author : Vikash Kumar Yadav
+# Date   : 15-04-2023
+# Topic  : First programme for the script
+
+#####################
+
+# this will print the hellow  world in the terminal 
+
+echo "Hello World"
